5071c6acc4e77dc060319c4453ea23301fa91117
İSRAFİL HALLAÇ <83336393+IsrafilHallac-ih@users.noreply.github.com>
2022-04-05 14:45:15 +0300

    Add files via upload
    DART BAŞLANGIÇ SEVİYESİ

---
+++ main.dart
@@ -0,0 +1,12 @@
+void main() {
+  String name = "İsrafil";
+  print("Merhaba " + name);
+
+  int number = 10;
+  double price = 24.5;
+  print("SAYI = " + number.toString());
+  print("SAYI = " + price.toString());
+
+  bool isTrue = 1000 > 3000;
+  print(isTrue);
+}
